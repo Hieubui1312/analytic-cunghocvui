ce137759d1b889651f3c94c7ae5a0a9b35625e21
HieuBUi <hieubui13121997@gmail.com>
2019-06-21 08:52:26 +0700

    Update config

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     return [
         "API_CLIENT" => [
-            'key_file_location' => storage_path('app/analytics/account.json') ,
+            'key_file_location' => storage_path('app/analytics/key.json') ,
             'application_name' => 'GA-CungHocVui',
             'scope' => ['https://www.googleapis.com/auth/analytics']
         ],
