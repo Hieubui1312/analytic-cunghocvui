399a22fc7660f57c0879a011fd794e1bb2ba2b98
HieuBui <hieubui13121997@gmail.com>
2019-06-28 21:40:02 +0700

    Sua route

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,7 +3,7 @@
     Route::middleware('web')->group(function (){
         Route::post('/postSearch',[
             'as'=> 'searchPath',
-            'uses' => \CungHocVui\Analytics\Controllers\AnalyticsController::class.'@postSearch'
+            'uses' => '\CungHocVui\Analytics\Controllers\AnalyticsController.@postSearch'
         ]);
-        Route::get('/index/cunghocvui', \CungHocVui\Analytics\Controllers\AnalyticsController::class."@index");
+        Route::get('/index/cunghocvui', "\CungHocVui\Analytics\Controllers\AnalyticsController@index");
     });
